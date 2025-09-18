cd44a0b39a9c75f50b3f337bb42d40d7245f02d1
Shivam Gupta <shivamgupta02005@gmail.com>
2025-09-18 12:30:10 +0530

    pring session on route page

--- src/app/dashboard/page.tsx
+++
@@ -1,6 +0,0 @@
-import { auth } from '../../auth';
-
-export default async function DashboardPage() {
-  const session = await auth();
-  return JSON.stringify(session);
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ import { auth } from '@/auth';
 export default async function Home() {
   const session = await auth();
   console.log(session);
-  return <div>Hello World</div>;
+  return <div>{JSON.stringify(session)}</div>;
 }
